6fb399b597e20ebd31b5499074a7b9699b4270e6
endtrastic <a.tuvikene@voco.ee>
2025-03-04 16:32:56 +0200

    Loginform authentication with JWT token, no css yet, and user creation

--- Backshots/backshots.js
+++ Backshots/backshots.js
@@ -81,6 +81,5 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
---
+++ TurboTuah/src/pages/about.jsx
@@ -0,0 +1,16 @@
+// Filename - pages/about.js
+
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>
+                TwinderTuah is a dating portal
+                for geeking midgets.
+            </h1>
+        </div>
+    );
+};
+
+export default About;
